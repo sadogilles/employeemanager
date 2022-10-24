e4c54e4a9e7561457d9711caa7dc069d6bb2a69f
sadogilles <sadowizzy@gmail.com>
2022-10-24 14:13:02 +0200

    created employee service, injected employeerepo with usage

--- src/main/java/fr/dosa/gillus/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/fr/dosa/gillus/employeemanager/repo/EmployeeRepo.java
@@ -3,6 +3,11 @@ package fr.dosa.gillus.employeemanager.repo;
 import fr.dosa.gillus.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Employee correspond to the class we want to create a repository for and Long is the type of the id attribute.
 public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+
+    void deleteEmployeeById(Long id); //spring is going to create a query based on the method name to delete the employee row
+
+    Employee findEmployeeById(Long id);
 
 }
---
+++ src/main/java/fr/dosa/gillus/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package fr.dosa.gillus.employeemanager.service;
+
+import fr.dosa.gillus.employeemanager.model.Employee;
+import fr.dosa.gillus.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService { // annote the service with @Service and autowired the repository with dependency injection
+
+    private final EmployeeRepo employeeRepo;
+    //dependency injection of the EmployeeRepo
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo){
+        this.employeeRepo=employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return this.employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id){
+         employeeRepo.deleteEmployeeById(id);
+    }
+
+    public Employee findEmployee(Long id){
+        return employeeRepo
+                .findEmployeeById(id);
+    }
+}
